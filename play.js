44ec5967e4e18fcbc3b4b3ec97b9260707a39216
surajjayraman <suraj.jayraman@gmail.com>
2021-08-25 15:20:01 -0400

    initial client set up

---
+++ play.js
@@ -0,0 +1,31 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // here we are sending data to the server
+  conn.on('connect', () => {
+    process.stdin.on('Hi There..', (data) => {
+      conn.write(data);
+    });
+  });
+
+  //handle incoming data
+  conn.on("data", (data) => {
+    // code that does something when the connection is first established
+    console.log(data);
+
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
